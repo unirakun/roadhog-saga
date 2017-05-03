42f643b9330ff52b369d054d61f75c1cf671dc84
Fabien JUIF <fabien.juif@zenika.com>
2017-05-03 16:35:45 +0200

    :white_check_mark: roadhog -init

---
+++ src/roadhog.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies, react/jsx-filename-extension */
+/* eslint-env jest */
+
+import roadhog from './roadhog'
+
+describe('roadhog', () => {
+  it('dummy - for coverage', () => {
+    roadhog()
+  })
+})
+
+/* eslint-enable import/no-extraneous-dependencies, react/jsx-filename-extension */
