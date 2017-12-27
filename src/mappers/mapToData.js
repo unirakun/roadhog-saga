454a406dbe46c948b79ba7414c221cfe0feab6f4
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2017-12-27 09:13:29 +0100

    :bug: return undefined when parsing json throws error

--- src/mappers/mapToData.js
+++ src/mappers/mapToData.js
@@ -1,5 +1,19 @@
 export default fallback => function* (raw) {
+  /* -ko- => fallback */
   if (!raw.ok && fallback) return fallback
-  if (raw.status !== 204) return yield raw.json()
-  return undefined
+  /* -no content- => undefined */
+  if (+(raw.status) === 204) return undefined
+
+  const contentType = raw.headers && raw.headers.get('Content-Type')
+  if (contentType && contentType.includes('json')) {
+    try {
+      /* json */
+      return yield raw.json()
+    } catch (e) {
+      /* json with parsing error => text */
+      return yield raw.text()
+    }
+  }
+
+  return yield raw.text()
 }
