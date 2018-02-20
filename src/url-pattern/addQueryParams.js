fd7d122387a8e1180ce86f14be1288604355c14d
vmenard <valentin.menard@zenika.com>
2018-02-20 16:00:39 +0100

    :bug: query param at the end of url (#33)

--- src/url-pattern/addQueryParams.js
+++ src/url-pattern/addQueryParams.js
@@ -6,7 +6,7 @@ export default url => (queryParams) => {
   const params = Object.keys(queryParams)
     .map((k => `${k}=${encodeParams(queryParams[k])}`))
 
-  const slash = (url.endsWith('/') || url.endsWith('&') || url.endsWith('?')) ? '' : '/'
+  const slash = (params.length > 0 || url.endsWith('/') || url.endsWith('&') || url.endsWith('?')) ? '' : '/'
   const questionMark = (url.endsWith('&') || url.endsWith('?')) ? '' : '?'
 
   return `${url}${slash}${questionMark}${params.join('&')}`
